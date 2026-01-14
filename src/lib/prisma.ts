433ef48ad3fd283f6d08fc7fb7d2163c0f85c424
Prince James Nagar <prince1.230101094@iiitbh.ac.in>
2026-01-15 00:56:37 +0530

    prisma-client-issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@/generated/prisma/client"
+// import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
